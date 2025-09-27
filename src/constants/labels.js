f507091cc60d4614b7ef9b8d5044998a5b05dac2
jayamohanan96 <jayamohanan96@gmail.com>
2025-09-27 11:57:44 +0530

    jaya

---
+++ src/constants/labels.js
@@ -0,0 +1,15 @@
+export const LABELS = {
+  assembly: {
+    en: 'Assembly',
+    ml: 'അസംബ്ലി'
+  },
+  district: {
+    en: 'District',
+    ml: 'ജില്ല'
+  },
+  state: {
+    en: 'State',
+    ml: 'സംസ്ഥാനം'
+  },
+  // ...add more as needed
+};
